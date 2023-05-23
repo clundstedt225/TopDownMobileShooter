f486dc51f07b6d9dbc4e0a262390fac2adb0ac6a
Connor Lundstedt <37668469+clundstedt225@users.noreply.github.com>
2023-05-23 04:33:47 -0400

    Started work on ignoring UI Joystick for aiming

--- surivialShooter/Assets/Scripts/PlayerAimWeapon.cs
+++ surivialShooter/Assets/Scripts/PlayerAimWeapon.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 //This script rotates and aims the weapon transform
 
@@ -12,18 +13,42 @@ public class PlayerAimWeapon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Get first touch and convert from pixel coords to world space
+        //Should we handle aiming logic
+        if (Input.touchCount > 0) { 
+            //Update weapon aim
+            HandleAiming();
+        }
+    }
+
+    void HandleAiming()
+    {
         Touch touch = Input.GetTouch(0);
         Vector3 fPos = Camera.main.ScreenToWorldPoint(touch.position);
         fPos.z = 0f;
 
-        //Get the player to finger direction
-        Vector3 aimDir = (fPos - transform.position).normalized;
+        //2d raycast (UI may be different???)
+        Vector2 touchPosWorld2D = new Vector2(fPos.x, fPos.y);
+        RaycastHit2D hitInformation = Physics2D.Raycast(touchPosWorld2D, Camera.main.transform.forward);
 
-        //Get angle in radians, then convert to degrees by multiplying
-        float angle = Mathf.Atan2(aimDir.y, aimDir.x) * Mathf.Rad2Deg;
+        //GameObject touchedObject = hitInformation.transform.gameObject;
 
-        //Apply rotation
-        aimTransform.eulerAngles = new Vector3(0, 0, angle);
-    }
+        if (hitInformation.collider != null)
+        {
+            Debug.Log("Hit info: " + hitInformation.collider.name);
+        }
+
+        //If not a joystick, then aim
+        if (!hitInformation || !hitInformation.transform.gameObject.GetComponent<FixedJoystick>())
+        {
+
+            //Get the player to finger direction
+            Vector3 aimDir = (fPos - transform.position).normalized;
+
+            //Get angle in radians, then convert to degrees by multiplying
+            float angle = Mathf.Atan2(aimDir.y, aimDir.x) * Mathf.Rad2Deg;
+
+            //Apply rotation
+            aimTransform.eulerAngles = new Vector3(0, 0, angle);
+        }
+    }         
 }
