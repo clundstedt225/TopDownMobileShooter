cc7cd92f994eda50151bb90fa332703df45d8288
Connor Lundstedt <37668469+clundstedt225@users.noreply.github.com>
2023-05-26 03:48:52 -0400

    More input code work

--- surivialShooter/Assets/Scripts/PlayerAimWeapon.cs
+++ surivialShooter/Assets/Scripts/PlayerAimWeapon.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 //This script rotates and aims the weapon transform
 
@@ -9,6 +10,17 @@ public class PlayerAimWeapon : MonoBehaviour
 {
 
     [SerializeField] private Transform aimTransform;
+
+    //UI raycast 
+    [SerializeField] private GraphicRaycaster m_Raycaster; //On canvas
+    [SerializeField] private EventSystem m_EventSystem; //In scene
+    private PointerEventData m_PointerEventData;
+
+    private void Start()
+    {
+        //Set up the new Pointer Event
+        m_PointerEventData = new PointerEventData(m_EventSystem);
+    }
 
     // Update is called once per frame
     void Update()
@@ -26,17 +38,42 @@ public class PlayerAimWeapon : MonoBehaviour
         Vector3 fPos = Camera.main.ScreenToWorldPoint(touch.position);
         fPos.z = 0f;
 
+        //Set the Pointer Event Position to that of the touch position
+        Vector2 touchPosWorld2D = new Vector2(fPos.x, fPos.y);
+        m_PointerEventData.position = touchPosWorld2D;
+
+        //Create a list of Raycast Results
+        List<RaycastResult> results = new List<RaycastResult>();
+
+        //Raycast using the Graphics Raycaster and mouse click position
+        m_Raycaster.Raycast(m_PointerEventData, results);
+
+        //For every result returned, output the name of the GameObject on the Canvas hit by the Ray
+        foreach (RaycastResult result in results)
+        {
+            Debug.Log("Hit " + result.gameObject.name);
+        }
+
+        /*
+
         //2d raycast (UI may be different???)
         Vector2 touchPosWorld2D = new Vector2(fPos.x, fPos.y);
         RaycastHit2D hitInformation = Physics2D.Raycast(touchPosWorld2D, Camera.main.transform.forward);
-
+        
         //GameObject touchedObject = hitInformation.transform.gameObject;
-
+        
         if (hitInformation.collider != null)
         {
             Debug.Log("Hit info: " + hitInformation.collider.name);
-        }
+        
+            if (hitInformation.transform.gameObject.GetComponent<FixedJoystick>())
+            {
+                Debug.Log("FOUND JOYSTICK!!!");
+            }
+        } 
+        */
 
+        /*
         //If not a joystick, then aim
         if (!hitInformation || !hitInformation.transform.gameObject.GetComponent<FixedJoystick>())
         {
@@ -49,6 +86,6 @@ public class PlayerAimWeapon : MonoBehaviour
 
             //Apply rotation
             aimTransform.eulerAngles = new Vector3(0, 0, angle);
-        }
-    }         
+        } */
+    }
 }
